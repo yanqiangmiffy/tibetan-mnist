0124cb0ca924be778fe9c0c7250b07f90c822f0a
yanqaingmiffy <1185918903@qq.com>
2018-12-05 21:48:27 +0800

    update cnn

--- train_cnn.py
+++ train_cnn.py
@@ -11,6 +11,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,Flatten
 from keras.layers import Conv2D,MaxPooling2D
+from keras.layers.normalization import  BatchNormalization
 from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
 from keras.optimizers import Adadelta
@@ -36,18 +37,25 @@ y_test = np_utils.to_categorical(y_test_, num_classes=10)
 input_shape=(img_rows,img_cols,1)
 model=Sequential()
 model.add(Conv2D(filters=32,kernel_size=(3,3),input_shape=input_shape,activation='relu'))
-model.add(Conv2D(filters=64,kernel_size=(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=32,kernel_size=(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(BatchNormalization())
 
-model.add(Dropout(0.25))
+model.add(Conv2D(filters=64,kernel_size=(3,3),activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(filters=64,kernel_size=(3,3),activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(BatchNormalization())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(n_classes, activation='softmax'))
 
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=Adadelta(),
+              optimizer='adam',
               metrics=['accuracy'])
 
 model.fit(X_train, y_train,
